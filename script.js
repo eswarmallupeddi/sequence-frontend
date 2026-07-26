aead6355ce2938576278ccdcf1c8672843f421f3
eswarmallupeddi <eswarmallupeddi@live.com>
2026-07-25 23:08:20 -0400

    Update backend URL in script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // REPLACE WITH YOUR SECURE CLOUDFLARE/ORACLE URL
-const backendUrl = "https://nickname-villas-smooth-online.trycloudflare.com"; 
+const backendUrl = "https://spec-beings-maiden-undertaken.trycloudflare.com"; 
 const socket = io(backendUrl);
 
 // Add these to check the pulse of the frontend
