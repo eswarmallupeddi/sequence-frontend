173b2547bb315b8e9f9aeddc84e13d51e619af73
eswarmallupeddi <eswarmallupeddi@live.com>
2026-07-26 18:06:10 -0400

    Update backend URL for socket connection

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const backendUrl = "https://spears-info-morris-portrait.trycloudflare.com"; 
+const backendUrl = "https://why-names-xbox-reflects.trycloudflare.com"; 
 const socket = io(backendUrl);
 
 socket.on('connect', () => console.log("🟢 Frontend connected!"));
