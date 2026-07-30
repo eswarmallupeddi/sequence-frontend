2cb62619d3c977be90c68f3c77b449aa6bd3adf1
eswarmallupeddi <eswarmallupeddi@live.com>
2026-07-30 19:30:00 -0400

    Update backend URL for socket connection

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const backendUrl = "https://door-notebooks-prozac-expo.trycloudflare.com"; 
+const backendUrl = "https://hitachi-slope-discretion-atlas.trycloudflare.com"; 
 
 const socket = io(backendUrl);
 
